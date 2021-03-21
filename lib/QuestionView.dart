43c5110e8346a426cca58a05ae8a921ba1186c81
ViniciusMacedo <vini.macedo.c@gmail.com>
2021-03-20 22:06:29 -0300

    % I adjusted the initializer | + added score counter

--- lib/QuestionView.dart
+++ lib/QuestionView.dart
@@ -10,15 +10,6 @@ import 'package:zallpy_quiz/QuestionData.dart';
 import 'dart:developer' as dev;
 import 'QuestionData.dart';
 
-// class QuestionView extends StatefulWidget {
-//   QuestionView({@required this.question});
-//
-//   final String question;
-//
-//   @override
-//   State createState() {}
-// }
-
 class QuestionView extends StatefulWidget {
   final List questionPos;
   final List question;
@@ -35,30 +26,40 @@ class _QuestionViewState extends State<QuestionView> {
   String viewQuestion;
   String viewCorrect;
   String viewAnswer;
+  int correctCounter = 0;
 
   // QuestionData questionData;
 
   @override
   void initState() {
     viewQuestion = widget.question.elementAt(widget.questionPos.elementAt(widget.inic));
+    print(viewQuestion);
     viewCorrect = widget.correct.elementAt(widget.questionPos.elementAt(widget.inic));
-    // viewAnswer = widget.answer.elementAt(widget.questionPos.elementAt(widget.inic))[0];
+    print(viewCorrect);
+    changeText();
   }
 
   changeText() {
-    print(widget.questionPos.elementAt(widget.inic));
-    print(widget.question.elementAt(widget.questionPos.elementAt(widget.inic)));
-    print(widget.correct.elementAt(widget.questionPos.elementAt(widget.inic)));
-    print(widget.answer.elementAt(widget.questionPos.elementAt(widget.inic))[0]);
-    if (widget.inic <= 4) {
+    if (widget.inic < 4) {
       setState(() {
         // this.test = widget.question[0]['question'].toString();
         this.viewQuestion = widget.question.elementAt(widget.questionPos.elementAt(widget.inic));
+        this.viewCorrect = widget.correct.elementAt(widget.questionPos.elementAt(widget.inic));
       });
       widget.inic++;
     } else {
       print('Para tudo');
+      print(this.correctCounter);
     }
+  }
+
+  score(int i) {
+    if (viewCorrect == widget.answer.elementAt(widget.questionPos.elementAt(widget.inic))[i]) {
+      this.correctCounter++;
+    } else {
+      print('errado\n');
+    }
+    changeText();
   }
 
   @override
@@ -72,7 +73,7 @@ class _QuestionViewState extends State<QuestionView> {
             padding: EdgeInsets.all(16.0),
             child: Center(
               child: Text(
-                'In which country\n' + viewQuestion + '\nwas created?',
+                'In which country\n' + viewQuestion + '\nwas created?\n$viewCorrect - $viewAnswer',
                 textAlign: TextAlign.center,
               ),
             ),
@@ -89,51 +90,25 @@ class _QuestionViewState extends State<QuestionView> {
                   ElevatedButton(
                     child: Text(widget.answer.elementAt(widget.questionPos.elementAt(widget.inic))[0]),
                     onPressed: () {
-                      if (viewCorrect == widget.answer.elementAt(widget.questionPos.elementAt(widget.inic))[0]) {
-                        print('correto\n');
-                      } else {
-                        print('errado\n');
-                        // print(snapshot.data.results.elementAt(0).correct);
-                      }
-                      changeText();
+                      score(0);
                     },
                   ),
                   ElevatedButton(
                     child: Text(widget.answer.elementAt(widget.questionPos.elementAt(widget.inic))[1]),
                     onPressed: () {
-                      if (viewCorrect == widget.answer.elementAt(widget.questionPos.elementAt(widget.inic))[1]) {
-                        print('correto\n');
-                      } else {
-                        print('errado\n');
-                        // print(snapshot.data.results.elementAt(0).correct);
-                      }
-                      changeText();
+                      score(1);
                     },
                   ),
                   ElevatedButton(
                     child: Text(widget.answer.elementAt(widget.questionPos.elementAt(widget.inic))[2]),
                     onPressed: () {
-                      // print(snapshot.data.results.elementAt(0).correct);
-                      // print(a.elementAt(2));
-                      if (viewCorrect == widget.answer.elementAt(widget.questionPos.elementAt(widget.inic))[2]) {
-                        print('correto\n');
-                      } else {
-                        print('errado\n');
-                        // print(snapshot.data.results.elementAt(0).correct);
-                      }
-                      changeText();
+                      score(2);
                     },
                   ),
                   ElevatedButton(
                     child: Text(widget.answer.elementAt(widget.questionPos.elementAt(widget.inic))[3]),
                     onPressed: () {
-                      if (viewCorrect == widget.answer.elementAt(widget.questionPos.elementAt(widget.inic))[3]) {
-                        print('correto\n');
-                      } else {
-                        print('errado\n');
-                        // print(snapshot.data.results.elementAt(0).correct);
-                      }
-                      changeText();
+                      score(3);
                     },
                   ),
                 ],
